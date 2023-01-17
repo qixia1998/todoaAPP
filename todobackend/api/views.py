82b7f5b0aa6a79ef18f2d4d99fb79443af4ce588
qixia1998 <qixia717138552@gmail.com>
2023-01-17 14:16:23 +0800

    Updated todobackend

--- todobackend/api/views.py
+++ todobackend/api/views.py
@@ -1,14 +1,20 @@
 from rest_framework import generics, permissions
 from .serializers import TodoSerializer, TodoToggleCompleteSerializer
 from todo.models import Todo
+from django.db import IntegrityError
+from django.contrib.auth.models import User
+from rest_framework.parsers import JSONParser
+from rest_framework.authtoken.models import Token
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import authenticate
 
 
 class TodoListCreate(generics.ListCreateAPIView):
-    # ListAPIView requires two mandatory attributes, serializer_class and
-    # queryset.
+    # ListAPIView requires two mandatory attributes, serializer_class and # queryset.
     # We specify TodoSerializer which we have earlier implemented
     serializer_class = TodoSerializer
-    permissions_class = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         user = self.request.user
@@ -38,5 +44,23 @@ class TodoToggleComplete(generics.UpdateAPIView):
         return Todo.objects.filter(user=user)
 
     def perform_update(self, serializer):
-        serializer.instance.completed=not(serializer.instance.completed)
+        serializer.instance.completed = not (serializer.instance.completed)
         serializer.save()
+
+
+@csrf_exempt
+def signup(request):
+    if request.method == 'POST':
+        try:
+            data = JSONParser().parse(request)  # data is a dictionary
+            user = User.objects.create_user(
+                username=data['username'],
+                password=data['password'])
+            user.save()
+
+            token = Token.objects.create(user=user)
+            return JsonResponse({'token': str(token)}, status=201)
+        except IntegrityError:
+            return JsonResponse(
+                {'error': 'username taken. choose another username'},
+                status=400)
